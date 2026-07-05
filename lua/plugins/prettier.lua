c395651d9a6d3f0b6494a9ba64b9fb4b5680cea7
origamid <andre@origamid.com>
2026-07-05 20:49:47 -0300

    port vscode prefs

---
+++ lua/plugins/prettier.lua
@@ -0,0 +1,14 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      javascript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescript = { "prettier" },
+      typescriptreact = { "prettier" },
+      css = { "prettier" },
+      html = { "prettier" },
+      json = { "prettier" },
+    },
+  },
+}
